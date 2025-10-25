e829a4e8d5c514863832c48b72f5aafe299b4824
PrimitiveTechExperience <119691704+PrimitiveTechExperience@users.noreply.github.com>
2025-10-25 17:22:42 -0400

    DynamoDB set up

--- backend/app.py
+++ backend/app.py
@@ -18,8 +18,23 @@ app = Flask(__name__)
 client = genai.Client(api_key=env.get("GEMINI_API_KEY"))
 CORS(app, origins=["http://localhost:3001"], supports_credentials=True)
 app.secret_key = env.get("APP_SECRET_KEY")
+dynamodb = boto3.resource('dynamodb', region_name='us-east-2')
+
+table = dynamodb.Table('ETA')
 
 @app.route("/generate", methods=["POST"])
+
+def store_user():
+    response = table.put_item(
+       Item={
+            'user_id': '123',
+            'name': 'John Doe',
+            'email': 'john.doe@example.com'
+            }
+        )
+    if response['ResponseMetadata']['HTTPStatusCode'] != 200:
+        return jsonify({"error": "Failed to store user"}), 500
+    return jsonify({"message": "User stored successfully"}), 200
 
 if __name__ == "__main__":
     # How to use the Gemini client to generate content.
