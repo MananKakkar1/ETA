00ea81859e9b11ae539056507413f2d1f39ea2d8
PrimitiveTechExperience <119691704+PrimitiveTechExperience@users.noreply.github.com>
2025-10-25 17:35:27 -0400

    Generating new user in the database done

--- backend/app.py
+++ backend/app.py
@@ -4,6 +4,8 @@ from os import environ as env
 from urllib.parse import quote_plus, urlencode
 from flask_cors import CORS
 import boto3
+import uuid
+import datetime
 
 from authlib.integrations.flask_client import OAuth
 from dotenv import load_dotenv, find_dotenv
@@ -22,19 +24,45 @@ dynamodb = boto3.resource('dynamodb', region_name='us-east-2')
 
 table = dynamodb.Table('ETA')
 
-@app.route("/generate", methods=["POST"])
-
-def store_user():
-    response = table.put_item(
-       Item={
-            'user_id': '123',
-            'name': 'John Doe',
-            'email': 'john.doe@example.com'
-            }
-        )
-    if response['ResponseMetadata']['HTTPStatusCode'] != 200:
-        return jsonify({"error": "Failed to store user"}), 500
-    return jsonify({"message": "User stored successfully"}), 200
+@app.route("/generate-user", methods=["POST"])
+def generate_new_user(eta_id, upload_date, name, email):
+    try:
+        data = request.get_json()
+        
+        if not data:
+            return jsonify({"error": "Invalid input"}), 400
+        
+        name = data.get("name")
+        email = data.get("email")
+        # eta_id = data.get("etaId")
+        # upload_date = data.get("uploadDate")
+        
+        if not all([name, email, eta_id, upload_date]):
+            return jsonify({"error": "Missing required fields"}), 400
+        
+        eta_id = str(uuid.uuid4())
+        upload_date = datetime.datetime.now().isoformat()
+        
+        response = table.put_item(
+        Item={
+                'ElectronicTeachingAssistantMaterialID': eta_id,
+                "UploadDate": upload_date,
+                'Name': name,
+                'Email': email,
+                'ChatHistory': [{'User':[], 'Assistant': []}],
+                'Context': [],
+                'Uploads': [],
+                }
+            )
+        if response['ResponseMetadata']['HTTPStatusCode'] != 200:
+            return jsonify({
+                "message": "User stored successfully",
+                "user_id": eta_id,
+                "upload_date": upload_date
+            }), 200
+        return jsonify({"message": "User stored successfully"}), 200
+    except Exception as e:
+        return jsonify({"error": str(e)}), 500
 
 if __name__ == "__main__":
     # How to use the Gemini client to generate content.
