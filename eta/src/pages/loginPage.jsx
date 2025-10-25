2869acb761294ef17db21ebd0c4392754f0d8648
kakkarm2 <manan.kakkar@mail.utoronto.ca>
2025-10-25 16:08:33 -0400

    Adding login page basic

--- eta/src/pages/loginPage.jsx
+++ eta/src/pages/loginPage.jsx
@@ -1,4 +1,26 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import App from "./App";
-import {Auth0Provider} from "@auth0/auth0-react";
+import React, { use } from "react";
+
+function LoginPage() {
+    let username, password = useState(getUserName, getPassword);
+
+    useEffect(() => {
+        username = getUserName();
+        password = getPassword();
+    }, []);
+    return <div>
+        <h1>Login Page</h1>
+        <form>
+            <label>
+                Username:
+                <input type="text" name="username" value={username} onChange={(e) => setUsername(e.target.value)} />
+            </label>
+            <br />
+            <label>
+                Password:
+                <input type="password" name="password" value={password} onChange={(e) => setPassword(e.target.value)} />
+            </label>
+            <br />
+            <button type="submit">Login</button>
+        </form>
+    </div>;
+}
